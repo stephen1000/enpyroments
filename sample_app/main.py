1ed2c83f7e9a15f6582a88d16d16263b3d8beb42
Stephen Lowery <slowery@markelcorp.com>
2019-07-22 17:06:35 -0400

    forcing the sys.path to work

--- sample_app/main.py
+++ sample_app/main.py
@@ -1,6 +1,14 @@
 import os
+import sys
 
-from enpyronments.loader import Loader
+try:
+    from enpyronments.loader import Loader
+except ImportError:
+    sys.path.append(
+        os.path.dirname(os.path.dirname(__file__))
+    )
+    from enpyronments.loader import Loader
+
 
 root = os.path.dirname(os.path.abspath(__file__))
 settings_dir = 'sample_settings'
@@ -9,6 +17,6 @@ settings = Loader(root).load_settings(settings_dir)
 
 if settings.DEBUG:
     print('---DEBUG MODE---')
-    print(settings.MESSAGE)
+    print(settings.WELCOME_MESSAGE)
 else:
-    print(settings.MESSAGE)
+    print(settings.WELCOME_MESSAGE)
