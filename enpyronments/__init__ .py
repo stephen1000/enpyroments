01a133a980d0b865f22ca6e847e83dad04a05395
Stephen Lowery <slowery@markelcorp.com>
2019-07-22 13:06:54 -0400

    +importing submodules -messing with sys.path

--- enpyronments/__init__ .py
+++ enpyronments/__init__ .py
@@ -1,4 +1,13 @@
+"""Enpyronments, a settings manager library
+"""
+
+
 import os
 import sys
 
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from enpyronments import loader, settings, utils
+
+# sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+
+__all__ = ['loader', 'settings', 'utils']
