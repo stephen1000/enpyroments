d781a50bc2cb3ce8c57f80e9baf3d17d0299bcf5
Stephen Lowery <slowery@markelcorp.com>
2019-07-22 11:02:59 -0400

    Extending from collections.MutableMapping instead of dict

--- enpyronments/settings.py
+++ enpyronments/settings.py
@@ -1,17 +1,35 @@
 import os
+from collections import MutableMapping
 
 from enpyronments.utils import Sensitive
 
 
-class Settings(dict):
+class Settings(MutableMapping):
     """Holder object for settings."""
 
+    def __init__(self, iterable=None, **kwargs):
+        """ Emulate dict.__init__'s style of accepting either an iterable of pairs or a set of keyword arguments """
+        if kwargs:
+            self.data = dict(**kwargs)
+        elif iterable:
+            self.data = dict(iterable)
+        else:
+            self.data = dict()
+
     def masked(self):
-        return dict(zip(self.keys(), (v.mask() if isinstance(v, Sensitive) else v for v in self.values())))
+        return dict(
+            zip(
+                self.data.keys(),
+                (
+                    v.mask() if isinstance(v, Sensitive) else v
+                    for v in self.values(return_obj=False)
+                ),
+            )
+        )
 
     def __getitem__(self, key):
         """ same as dict.__getitem__, but extracts the value of Sensitive type elements """
-        val = super().__getitem__(key)
+        val = self.data.__getitem__(key)
         if isinstance(val, Sensitive):
             return val.obj
         return val
@@ -19,36 +37,55 @@ class Settings(dict):
     def __setitem__(self, key, val):
         """ Same as dict.__setitem__, but sets the value of Sensitive type elements if the current value is already
         Sensitive """
-        if key in self and isinstance(self[key], Sensitive):
-            return super().__setitem__(key, Sensitive(val))
-        return super().__setitem__(key, val)
+        # Don't want to set a Sensitive value to a nonsensitive value, so if:
+        # 1- The key we're setting is already set
+        # 2- The value we're setting isn't already sensitive
+        # 3- The value we already have is sensitive
+        # Then we need to wrap val in Sensitive().
 
-    def get(self, key, default=None):
+        if (
+            key in self.data
+            and not isinstance(val, Sensitive)
+            and isinstance(self.get(key, extract_from_sensitive=False), Sensitive)
+        ):
+            return self.data.__setitem__(key, Sensitive(val))
+        return self.data.__setitem__(key, val)
+
+    def __delitem__(self, key):
+        """ Same as dict.__delitem__ """
+        return self.data.__delitem__(key)
+
+    def __iter__(self):
+        """ Same as dict.__iter__ """
+        yield self.data.__iter__()
+
+    def __len__(self):
+        """ Same as dict.__len__ """
+        return self.data.__len__()
+
+    def get(self, key, default=None, extract_from_sensitive: bool = True):
         """ Same as dict.get, but extracts the value of Sensitive type elements """
-        try:
-            val = self[key]
-        except KeyError:
-            return default
+        val = self.data.get(key)
 
-        if isinstance(val, Sensitive):
+        if extract_from_sensitive and isinstance(val, Sensitive):
             return val.obj
 
         return val
 
-    def items(self):
+    def items(self, extract_from_sensitive: bool = True) -> tuple:
         """ Same as dict.items, but extracts the value of Sensitive type elements """
-        for key, val in super().items():
-            if isinstance(val, Sensitive):
+        for key, val in self.data.items():
+            if extract_from_sensitive and isinstance(val, Sensitive):
                 yield key, val.obj
             yield key, val
 
-    def values(self):
+    def values(self, extract_from_sensitive: bool = True) -> tuple:
         """ Same as dict.values, but extracts the value of Sensitive type elements """
-        for val in super().values():
-            if isinstance(val, Sensitive):
+        for val in self.data.values():
+            if extract_from_sensitive and isinstance(val, Sensitive):
                 yield val.obj
             yield val
 
     def save_to_environ(self):
         """Saves the current state of settings to the environment via os.environ"""
-        os.environ.update(self)
+        os.environ.update(self.data)
