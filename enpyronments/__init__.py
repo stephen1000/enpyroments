fb20c3cced3fb452b610532222d63782028a6b3c
Stephen Lowery <slowery@markelcorp.com>
2019-07-22 16:01:32 -0400

    so __init__ .py isn't __init__.py...

--- enpyronments/__init__.py
+++ enpyronments/__init__.py
@@ -7,7 +7,6 @@ import sys
 
 from enpyronments import loader, settings, utils
 
-# sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+__all__ = ["loader", "settings", "utils"]
 
-
-__all__ = ['loader', 'settings', 'utils']
+foo = "bar"
